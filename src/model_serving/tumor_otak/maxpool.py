35bd02b83c951d38ccecc25c2100ac7ea17f37d6
Rakhid16 <roploverz@gmail.com>
2020-12-13 12:46:22 +0700

    maxpooling layer added

---
+++ src/model_serving/tumor_otak/maxpool.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class MaxPool2:
+  def iterate_regions(self, image):
+    h, w, _ = image.shape
+    new_h = h // 2
+    new_w = w // 2
+
+    for i in range(new_h):
+      for j in range(new_w):
+        im_region = image[(i * 2):(i * 2 + 2), (j * 2):(j * 2 + 2)]
+        yield im_region, i, j
+
+  def forward(self, input):
+    h, w, num_filters = input.shape
+    output = np.zeros((h // 2, w // 2, num_filters))
+
+    for im_region, i, j in self.iterate_regions(input):
+      output[i, j] = np.amax(im_region, axis=(0, 1))
+
+    return output
